59c27a450f7131834ddc977945f1be12e18c535b
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-03 18:42:54 +0700

    feat<>: middleware for getMe route

---
+++ backend/middleware/protectedRoute.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
+
+
+const protectedRoute = async(req, res, next) => {
+   try {
+      const token = req.cookies.jwt;
+      if(!token) {
+         res.status(401).json({ error: 'Unauthorized: No token provided' });
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if(!decoded) {
+         res.status(401).json({ error: 'Unauthorized: Invalid token' });
+      }
+
+      const user = await User.findById(decoded.userId).select('-password');
+      if(!user) {
+         res.status(404).json({ error: 'User not found' })
+      }
+
+      req.user = user;
+      next();
+   } catch(err) {
+      console.log(`Error in protectRoute middleware ${err.message}`);
+      res.status(500).json({ error: 'Internal server error' });
+   }
+}
