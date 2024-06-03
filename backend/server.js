01bfb8acfacd2f965391e7375ce6175c625fd13c
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-03 07:35:29 +0700

    feat<>: add auth.routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('run')
+import express from 'express';
+import authRoutes from './routes/auth.routes';
+
+
+const app = express();
+const port = 3000;
+const host = 'localhost'
+
+app.use('/api/auth', authRoutes);
+
+app.listen(port, () => {
+   console.log(`Server is running on ${host}:${port}`)
+})
