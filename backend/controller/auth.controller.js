24a180de72005f0b39b4f90d7cb752131b066925
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-03 18:39:40 +0700

    feat<>: create signup and login function controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,15 +1,110 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async(req, res) => {
-   res.json({
-      data: 'you hit the signup endpoint'
-   })
+   try {
+      const { fullName, username, email, password } = req.body;
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(!emailRegex.test(email)) {
+         return res.status(400).json({ error: "Invalid email format" });
+      }
+
+      const existingUser = await User.findOne({ username });
+      if(existingUser) {
+         return res.status(400).json({ error: "Username already taken" });
+      }
+
+      const existingEmail = await User.findOne({ email });
+      if(existingEmail) {
+         return res.status(400).json({ error: "Email already taken" });
+      }
+
+      const salt = await bcrypt.genSalt(10); // define hash length
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const newUser = new User({
+         fullName,
+         username,
+         email,
+         password: hashedPassword,
+      })
+
+      if(newUser) {
+         generateTokenAndSetCookie(newUser._id, res);
+         await newUser.save();
+
+         res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            userName: newUser.username,
+            email: newUser.email,
+            followers: newUser.followers,
+            following: newUser.following,
+            profileImg: newUser.profileImg,
+            coverImg: newUser.coverImg,
+         })
+      } else {
+         res.status(400).json({ error: 'Invalid user data'});
+      }
+   } catch(err) {
+      console.log(`Error in signup controller: ${err.message}`);
+      res.status(500).json({ 
+         error: `Internal server error occured: ${err.message}` 
+      })
+   }
 }
 export const login = async(req, res) => {
-   res.json({
-      data: 'you hit the login endpoint'
-   })
+   try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      const isPasswordValid = await bcrypt.compare(password, user?.password || "")
+
+      if(!user || !isPasswordValid) {
+         return res.status(400).json({ error: `Invalid credentials` });
+      }
+
+      generateTokenAndSetCookie(user._id, res);
+
+      res.status(200).json({
+         _id: user._id,
+         fullname: user.fullName,
+         username: user.username,
+         email: user.email,
+         followers: user.followers,
+         following: user.following,
+         profileImg: user.profileImg,
+         coverImg: user.coverImg,
+      })
+
+   } catch(err) {
+      console.log(`Error in login controller: ${err.message}`)
+      res.status(500).json({
+         error: `Internal server error occured: ${err.message}`
+      })
+   }
 }
+
+export const getMe = async(req, res) => {
+   try {
+
+   } catch(err) {
+      
+   }
+}
+
+
 export const logout = async(req, res) => {
-   res.json({
-      data: 'you hit the logout endpoint'
-   })
+   try {
+      
+
+
+
+   } catch(err) {
+      console.log(`Error in logout controller: ${err.message}`);
+      res.status(500).json({
+         error: `Internal server error occured: ${err.message}`
+      });
+   }
 }
