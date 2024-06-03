fd63d7b1354c39cbcda402a074b40ce0f27a1c68
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-03 18:38:43 +0700

    feat<app.use> add cookieParser and urlecoded middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongodb.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 
@@ -11,6 +12,8 @@ const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse request.body
+app.use(express.urlencoded({ extended: true })) // to parse form data
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
