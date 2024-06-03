313bce10050bba4793ef8642a8dfae6e5861963f
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-03 18:41:35 +0700

    feat<router>: add get route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {  login, logout, signup } from '../controller/auth.controller.js'
+import {  getMe, login, logout, signup } from '../controller/auth.controller.js'
 
 const router = express.Router();
 
+router.get('/me', protectedRoute, getMe);
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
