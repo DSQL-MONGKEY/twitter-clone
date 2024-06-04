8819565443468795cb105d2cda365f8c4539d073
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-04 18:20:38 +0700

    feat<>: add user update route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { followUnfollowUser, getUserProfile } from '../controller/user.controller.js';
+import { followUnfollowUser, getSuggestedUser, getUserProfile } from '../controller/user.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.get('/profile/:username', protectRoute, getUserProfile);
-// router.get('/suggested', , getUserProfile);
+router.get('/suggested', protectRoute, getSuggestedUser );
 router.post('/follow/:id', protectRoute, followUnfollowUser);
 // router.post('/update', updateUserProfile);
 
