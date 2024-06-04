d7c8d474b5076b579befd6982ee7092cc9d8c7ea
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-04 18:23:40 +0700

    feat<updateUser>: add update password functional

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import notifications from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
@@ -61,3 +62,62 @@ export const followUnfollowUser = async(req, res) => {
       res.status(500).json({ error: err.message });
    }
 }
+
+export const getSuggestedUser = async(req, res) => {
+   try {
+      const userId = req.user._id;
+      const userFollowedByMe = await User.findById(userId).select("following");
+
+      const users = await User.aggregate([
+         {
+            $match: {
+               _id: { $ne: userId },
+            },
+         },
+         { $sample: { size: 10 } },
+      ]);
+
+      // Filter the suggested users that haven't followed
+      const filteredUsers = users.filter(user => !userFollowedByMe.following.includes(user._id));
+      const suggestedUsers = filteredUsers.slice(0, 4);
+
+      suggestedUsers.forEach(user => (user.password = null));
+      res.status(200).json(suggestedUsers);
+   } catch(err) {
+      console.log(`Error in getSuggestedUser ${err.message}`);
+      res.status(500).json({ error: err.message });
+   }
+}
+
+export const updateUser = async(req, res) => {
+   const { fullName, username, email, currentPassword, newPassword, bio, link } = req.body;
+   let { profileImg, coverImg } = req.body;
+
+   const userId = req.user._id;
+
+   try {
+      const user = await User.findById(userId);
+      if(!user) return res.status(400).json({ error: "User not found" });
+
+      if((!newPassword && newPassword) || (!newPassword && currentPassword)) {
+         return res.status(400).json({ error: 'Please provide both current password and new password' });
+      }
+
+      if(currentPassword && newPassword) {
+         const isMatch = await bcrypt.compare(currentPassword, user.password);
+         if(!isMatch) {
+            return res.status(400).json({ error: 'Current password is incorrect' });
+         }
+         if(newPassword.length < 6) {
+            return res.status(400).json({ error: 'Password must be at least 6 characters long' });
+         }
+
+         const salt = await bcrypt.genSalt(10);
+         user.password = await bcrypt.hash(newPassword, salt);
+      }
+   } catch(err) {
+      console.log(`Error in updateUser controller: ${err.message}`);
+      res.status(500).json({ error: err.message });
+   }
+
+} 
