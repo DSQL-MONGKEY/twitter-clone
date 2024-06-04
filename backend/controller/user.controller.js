86167a84d91e4ab2552a2b192b0828413ab6c515
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-04 21:56:24 +0700

    feat<updateUser>: add upload validation and change the const user into let user

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
 import notifications from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
@@ -96,7 +97,7 @@ export const updateUser = async(req, res) => {
    const userId = req.user._id;
 
    try {
-      const user = await User.findById(userId);
+      let user = await User.findById(userId);
       if(!user) return res.status(400).json({ error: "User not found" });
 
       if((!newPassword && newPassword) || (!newPassword && currentPassword)) {
@@ -115,6 +116,34 @@ export const updateUser = async(req, res) => {
          const salt = await bcrypt.genSalt(10);
          user.password = await bcrypt.hash(newPassword, salt);
       }
+
+      if(profileImg) {
+         if(user.profileImg) {
+            await cloudinary.uploader.destroy(user.profileImg.split('/').pop().split('.')[0]);
+         }
+
+         const uploadedResponse =  await cloudinary.uploader.upload(profileImg);
+         profileImg = uploadedResponse.secure_url;
+      }
+
+      if(coverImg) {
+         const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+         coverImg = uploadedResponse.secure_url;
+      }
+
+      user.fullName = fullName || user.fullName;
+      user.username = username || user.username;
+      user.email = email || user.email;
+      user.bio = bio || user.bio;
+      user.link = link || user.link;
+      user.profileImg = profileImg || user.profileImg;
+      user.coverImg == coverImg || user.coverImg;
+
+      user = await user.save();
+      // Password must be null in response
+      user.password = null;
+
+      return res.status(200).json(user);
    } catch(err) {
       console.log(`Error in updateUser controller: ${err.message}`);
       res.status(500).json({ error: err.message });
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { followUnfollowUser, getSuggestedUser, getUserProfile } from '../controller/user.controller.js';
+import { followUnfollowUser, getSuggestedUser, getUserProfile, updateUser } from '../controller/user.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/profile/:username', protectRoute, getUserProfile);
 router.get('/suggested', protectRoute, getSuggestedUser );
 router.post('/follow/:id', protectRoute, followUnfollowUser);
-// router.post('/update', updateUserProfile);
+router.post('/update', protectRoute, updateUser);
 
 
 export default router;
