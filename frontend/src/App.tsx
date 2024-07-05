62cc1df23d48fef07588e57d48f6dd9c9a13c148
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-05 20:07:11 +0700

    feat<Route>: add notifications and profile route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import HomePage from './pages/home/HomePage'
 import LoginPage from './pages/auth/login/LoginPage'
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
       </Routes>
       <RightPanel />
     </div>
