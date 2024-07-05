47f5cc009ffa1f81fda7bf861374442202ae23d9
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-05 20:07:52 +0700

    initial create

---
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,22 @@
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Posts from "../../components/common/Posts";
+import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
+import EditProfileModal from "./EditProfileModal";
+
+import { POSTS } from "../../utils/db/dummy";
+
+import { FaArrowLeft } from "react-icons/fa6";
+import { IoCalendarOutline } from "react-icons/io5";
+import { FaLink } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+const ProfilePage = () => {
+   return (
+      <div className='flex-[4_4_0]'>
+
+      </div>
+   )
+}
+
+export default ProfilePage
