3c2fced2f643d2b56e42c2695fe1c00873ca6799
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-09 20:20:53 +0700

    feat<>: add updatedComments for return actual value to the client

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -100,8 +100,9 @@ export const commentOnPost = async(req, res) => {
       const comment = { user: userId, text }
       post.comments.push(comment);
       await post.save();
-
-      res.status(201).json(post);
+      
+      const updatedComments = post.comments
+      res.status(201).json(updatedComments);
    } catch(err) {
       res.status(500).json({ error: 'Internal server error' });
       console.log(`Error in commentOnPost controller: ${err}`);
