345cfb0920108b8324aae1a6b55ad2dc89ce4cbd
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-10 11:06:12 +0700

    chore<Notification>: correcting the spelling of notifications to Notification model

--- backend/controller/notification.controller.js
+++ backend/controller/notification.controller.js
@@ -1,16 +1,16 @@
-import Notifications from "../models/notification.model.js";
+import Notification from "../models/notification.model.js";
 
 
 
 export const getNotifications = async(req, res) => {
    try {
       const userId = req.user._id;
-      const notifications = await Notifications.find({ to: userId })
+      const notifications = await Notification.find({ to: userId })
          .populate({
             path: 'from',
             select: 'username profileImg',
          })
-      await Notifications.updateMany({ to: userId }, { read: true });
+      await Notification.updateMany({ to: userId }, { read: true });
 
       res.status(200).json(notifications);
    } catch(err) {
@@ -23,7 +23,7 @@ export const deleteNotifications = async(req, res) => {
    try {
       const userId = req.user._id;
 
-      await Notifications.deleteMany({ to: userId });
+      await Notification.deleteMany({ to: userId });
 
       res.status(200).json({ message: 'Notifications deleted successfully' });
    } catch(err) {
