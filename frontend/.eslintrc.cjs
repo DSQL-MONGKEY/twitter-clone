e29ba81b6098852f4fe9263c427770b8fca51043
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-14 20:15:47 +0700

    explict any

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  "rules": {
+    "@typescript-eslint/no-explicit-any": false
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
