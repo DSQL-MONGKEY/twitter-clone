95b2a38ec89b1d56eb5aa498748cbb863ee4eb35
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-20 22:17:07 +0700

    feat<>: change api endpoint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       port: 3000,
       proxy: {
          "/api" : {
-            target: "http://localhost:5000",
+            target: "https://twitter-backend-m7b6mm5b4a-as.a.run.app",
             changeOrigin: true,
          }
       }
