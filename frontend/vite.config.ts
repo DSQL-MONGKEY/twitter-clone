be7703b8d3be60d779130b3a2b7fc6597a794354
Dimpfe <dimasprasetyo7696@gmail.com>
2024-07-26 05:40:01 +0700

    build<>: change to localhost for dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       port: 3000,
       proxy: {
          "/api" : {
-            target: "https://twitter-backend-m7b6mm5b4a-as.a.run.app",
+            target: "localhost:5000",
             changeOrigin: true,
          }
       }
